cf4a21ae9d538efb32f86e30b6f23f23e20b47f4
Nat Steven <nathanaelsteven@boisestate.edu>
2024-07-09 15:44:39 -0600

    issue hopefulyl resolved

--- job_slurm.sh
+++ job_slurm.sh
@@ -5,13 +5,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
 #SBATCH -t 12:00:00
-#SBATCH --array=0-307
+#SBATCH --array=0-230
 #SBATCH --output=logs/slurm-%A_%a.out
 
 module load apptainer/1.2.5
 
-declare -A extensions=( ["mas"]="json" ["z3"]="smt2" ["cvc5"]="smt2" ["ostrich"]="smt2" )
-solvers=("mas" "z3" "cvc5" "ostrich")
+declare -A extensions=( ["mas"]="json" ["z3"]="smt2" ["ostrich"]="smt2" )
+solvers=("mas" "z3" "ostrich")
 readarray -t files < util/filenames.txt
 mkdir -p logs
 
@@ -24,4 +24,4 @@ file=${files[$file_index]}
 
 mkdir -p logs/"$solver"
 
-srun ./solver_run.sh "$solver" "benches/real/$solver/$file.$file_extension"
+srun ./solver_run.sh "$solver" "real/$solver/$file.$file_extension"
