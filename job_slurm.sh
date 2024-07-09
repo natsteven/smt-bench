ea8ce172a80805fc444bfdd958fd610c4d2a7f8f
natsteven <nathanaelsteven@gmail.com>
2024-07-08 18:15:06 -0600

    slurm working and good benches

--- job_slurm.sh
+++ job_slurm.sh
@@ -2,29 +2,36 @@
 #SBATCH -J SMT_BENCH		#job name
 #SBATCH -p bsudfq		    #queue
 #SBATCH --nodes=1
-#SBATCH --ntasks=12
-#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
 #SBATCH -t 12:00:00
+#SBATCH --array=0-19
+#SBATCH --output=logs/slurm-%A_%a.out
 
-echo "Date              = $(date)"
-echo "Hostname          = $(hostname -s)"
-echo "Working Directory = $(pwd)"
-echo ""
-echo "Number of Nodes Allocated  = $SLURM_JOB_NUM_NODES"
-echo "Number of Tasks Allocated  = $SLURM_NTASKS"
-echo ""
+#echo "Date              = $(date)"
+#echo "Hostname          = $(hostname -s)"
+#echo "Working Directory = $(pwd)"
+#echo ""
+#echo "Number of Nodes Allocated  = $SLURM_JOB_NUM_NODES"
+#echo "Number of Tasks Allocated  = $SLURM_NTASKS"
+#echo ""
 
 module load apptainer/1.2.5
 
+declare -A extensions=( ["mas"]="json" ["z3"]="smt2" ["cvc5"]="smt2" ["ostrich"]="smt2" )
 solvers=("mas" "z3" "cvc5" "ostrich")
+readarray -t files < filenames.txt
 mkdir -p logs
-for solver in "${solvers[@]}"; do
-  mkdir -p logs/"$solver"
-  performance_log="logs/${solver}_performance.log"
-  echo "Performance log for $solver" > "$performance_log"
-  for file in simple-benches/"$solver"/*; do
-    srun --exclusive ./solver_run.sh "$solver" "$file"
-  done
-done
 
+solver_index=$((${SLURM_ARRAY_TASK_ID} / ${#files[@]}))
+file_index=$((${SLURM_ARRAY_TASK_ID} % ${#files[@]}))
 
+solver=${solvers[$solver_index]}
+file_extension=${extensions[$solver]}
+file=${files[$file_index]}
+
+mkdir -p logs/"$solver"
+#performance_log="logs/${solver}_performance.log"
+#echo "Performance log for $solver" > "$performance_log"
+
+srun ./solver_run.sh "$solver" "simple-benches/$solver/$file.$file_extension"
--- solver_run.sh
+++ solver_run.sh
@@ -3,15 +3,20 @@
 solver="$1"
 file="$2"
 log="logs/$solver/$(basename "$file").log"
-performance_log="logs/${solver}_performance.log"
+performance_log="logs/$solver/$(basename "$file").time"
 
-echo "Running $solver on $(basename "$file")"
-echo "$file" >> "$performance_log"
+echo -n "$solver, $(basename "$file"), "
+#echo "$file" >> "$performance_log"
 
-(time timeout 20m apptainer run --app "$solver" solver-bench.sif "$file" > "$log") 2>> "$performance_log"
+start=$(date +%s.%3N)
+
+(time timeout 20m apptainer run --app "$solver" solver-bench.sif "$file" > "$log") 2> "$performance_log"
 if [ $? -eq 124 ]; then
-  echo "Timeout: Solver $solver exceeded 20 minutes on file $(basename "$file")" >> "$performance_log"
+  echo "Timeout: Solver $solver exceeded 20 minutes on file $(basename "$file")" > "$performance_log"
 fi
 if [ "$solver" = "mas" ]; then
   cp temp/solutions.txt logs/"$solver"/"$(basename "$file")".solutions.txt
 fi
+
+end=$(date +%s.%3N)
+echo "$(echo "$end - $start" | bc)"
