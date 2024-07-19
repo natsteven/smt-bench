fe0f338339060438a7f9fc1f7c76410bf27e72a1
Nat Steven <nathanaelsteven@boisestate.edu>
2024-07-19 12:03:14 -0600

    woorpje benching ready

--- job_slurm.sh
+++ job_slurm.sh
@@ -5,18 +5,18 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
 #SBATCH -t 12:00:00
-#SBATCH --array=0-230
+#SBATCH --array=0-2435
 #SBATCH --output=logs/slurm-%A_%a.out
 
 module load apptainer/1.2.5
 
-declare -A extensions=( ["mas"]="json" ["z3"]="smt2" ["ostrich"]="smt2" )
-solvers=("mas" "z3" "ostrich")
+declare -A extensions=( ["mas"]="smt2.json" ["z3"]="smt2" ["ostrich"]="smt2" ["cvc5"]="smt2" )
+solvers=("mas" "z3" "ostrich" "cvc5")
 readarray -t files < util/filenames.txt
 mkdir -p logs
 
-solver_index=$((${SLURM_ARRAY_TASK_ID} / ${#files[@]}))
-file_index=$((${SLURM_ARRAY_TASK_ID} % ${#files[@]}))
+solver_index=$((SLURM_ARRAY_TASK_ID / ${#files[@]}))
+file_index=$((SLURM_ARRAY_TASK_ID % ${#files[@]}))
 
 solver=${solvers[$solver_index]}
 file_extension=${extensions[$solver]}
@@ -24,4 +24,4 @@ file=${files[$file_index]}
 
 mkdir -p logs/"$solver"
 
-srun ./solver_run.sh "$solver" "real/$solver/$file.$file_extension"
+srun ./solver_run.sh "$solver" "smt-comp/$solver/woorpje/$file.$file_extension"
