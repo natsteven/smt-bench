d0b99e580d6b407e11ee223534e3ebf176bd8f74
natsteven <nathanaelsteven@gmail.com>
2024-07-20 16:44:31 -0600

    woorpje results and sygus prep

--- job_slurm.sh
+++ job_slurm.sh
@@ -5,14 +5,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
 #SBATCH -t 12:00:00
-#SBATCH --array=0-2435
+#SBATCH --array=0-1028
 #SBATCH --output=logs/slurm-%A_%a.out
 
 module load apptainer/1.2.5
 
 declare -A extensions=( ["mas"]="smt2.json" ["z3"]="smt2" ["ostrich"]="smt2" ["cvc5"]="smt2" )
-solvers=("mas" "z3" "ostrich" "cvc5")
-readarray -t files < util/filenames.txt
+solvers=( "z3" "ostrich" "cvc5")
+readarray -t files < util/sygus-filenames.txt
 mkdir -p logs
 
 solver_index=$((SLURM_ARRAY_TASK_ID / ${#files[@]}))
@@ -24,4 +24,4 @@ file=${files[$file_index]}
 
 mkdir -p logs/"$solver"
 
-srun ./solver_run.sh "$solver" "smt-comp/$solver/woorpje/$file.$file_extension"
+srun ./solver_run.sh "$solver" "smt-comp/$solver/sygus/$file.$file_extension"
