fe3e8f329e99116dfc0ade246dbfd568264dab87
Nat Steven <nathanaelsteven@boisestate.edu>
2024-07-23 15:44:06 -0600

    getting good

---
+++ GetGood.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class GetGood {
+
+    public static void main (String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java GetGood <input>");
+            System.exit(0);
+        }
+        try {
+            FileReader input = new FileReader(new File(args[0]));
+            Scanner scanner = new Scanner(input);
+
+            HashSet<String> bad = new HashSet<>(Arrays.asList("re.union", "re.+", "re.loop", "re.range", "re.opt", "re.comp"));
+            boolean good = true;
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] info = line.split(":");
+                String file = info[0];
+                String funs = info[1];
+                String[] list = funs.split(",");
+                for (String fun : list) {
+                    if (bad.contains(fun.trim())){
+                        good = false;
+                        break;
+                    }
+                }
+                if (good) System.out.println(file+ ".smt2");
+                else good = true;
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
