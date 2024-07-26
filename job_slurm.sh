bdbb06f3d6765d1852c833c77014d24bbe145e4d
Nat Steven <nathanaelsteven@boisestate.edu>
2024-07-26 12:14:12 -0600

    simple benches

--- job_slurm.sh
+++ job_slurm.sh
@@ -5,20 +5,20 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
 #SBATCH -t 12:00:00
-#SBATCH --array=0-2435
+#SBATCH --array=0-51
 #SBATCH --output=logs/slurm-%A_%a.out
 
 module load apptainer/1.2.5
 
 declare -A extensions=( ["mas"]="smt2.json" ["z3"]="smt2" ["ostrich"]="smt2" ["cvc5"]="smt2" )
 solvers=( "mas" "z3" "ostrich" "cvc5")
-benches=( "woorpje" "slog" "sygus" )
+#benches=( "woorpje" "slog" "sygus" )
 files=()
-for bench in "${benches[@]}"; do
-	readarray -t tfiles < util/"$b"-filenames.txt
-	files+=("${tfiles[@]}")
-done
-#readarray -t files < util/woorpje-filenames.txt
+#for bench in "${benches[@]}"; do
+#	readarray -t tfiles < util/"$b"-filenames.txt
+#	files+=("${tfiles[@]}")
+#done
+readarray -t files < util/simple-filenames.txt
 mkdir -p logs
 
 solver_index=$((SLURM_ARRAY_TASK_ID / ${#files[@]}))
@@ -27,8 +27,8 @@ file_index=$((SLURM_ARRAY_TASK_ID % ${#files[@]}))
 solver=${solvers[$solver_index]}
 file_extension=${extensions[$solver]}
 file=${files[$file_index]}
-bench=${benches[$((file_index / $]}
+#bench=${benches[$((file_index / $]}
 
 mkdir -p logs/"$solver"
 
-srun ./solver_run.sh "$solver" "smt-comp/$solver/woorpje/$file.$file_extension"
+srun ./solver_run.sh "$solver" "simple-benches/$solver/$file.$file_extension"
