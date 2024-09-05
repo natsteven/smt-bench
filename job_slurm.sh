e9075206fe92a1cfbdd6915afdc7fc549ad74c4c
natsteven <nathanaelsteven@gmail.com>
2024-09-04 21:42:06 -0600

    mas automatark

--- job_slurm.sh
+++ job_slurm.sh
@@ -5,20 +5,20 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
 #SBATCH -t 12:00:00
-#SBATCH --array=0-608
+#SBATCH --array=0-7899
 #SBATCH --output=logs/slurm-%A_%a.out
 
 
 benches="smt-comp"
-subdir="/woorpje" #include leading slash
+subdir="/slog" #include leading slash
 
 filenames="util/$benches$subdir-filenames.txt"
-lengths="util/$benches$subdir-lengths.txt"
+#lengths="util/$benches$subdir-lengths.txt"
 
 module load apptainer/1.2.5
 
 declare -A extensions=( ["mas"]="smt2.json" ["z3"]="smt2" ["ostrich"]="smt2" ["cvc5"]="smt2" )
-solvers=( "mas" )
+solvers=( "mas" "cvc5" "ostrich" "z3" )
 #benches=( "woorpje" "slog" "sygus" )
 #files=()
 #for bench in "${benches[@]}"; do
@@ -26,7 +26,7 @@ solvers=( "mas" )
 #	files+=("${tfiles[@]}")
 #done
 readarray -t files < $filenames
-readarray -t lens < $lengths
+#readarray -t lens < $lengths
 mkdir -p logs
 
 solver_index=$((SLURM_ARRAY_TASK_ID / ${#files[@]}))
@@ -35,16 +35,19 @@ file_index=$((SLURM_ARRAY_TASK_ID % ${#files[@]}))
 solver=${solvers[$solver_index]}
 file_extension=${extensions[$solver]}
 file=${files[$file_index]}
-len=${lens[$file_index]}
+#len=${lens[$file_index]}
 #bench=${benches[$((file_index / $]}
 
-if [ "$len" == "" ]; then
-	len=15
-fi
-if [ "$len" -gt 15 ]; then
-	len=15
-fi
+#if [ "$len" == "" ]; then
+#	len=15
+#fi
+#if [ "$len" -gt 15 ]; then
+#	len=15
+#fi
+#if [ "$len" -lt 1 ]; then
+#	len=1
+#fi
 
 mkdir -p logs/"$solver"
 
-srun ./solver_run.sh "$solver" "$benches/$solver$subdir/$file.$file_extension" $len
+srun ./solver_run.sh "$solver" "$benches/$solver$subdir/$file.$file_extension" 16
