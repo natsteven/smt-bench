bc224522f1ac7885900bed00fe746b51341fe48e
natsteven <nathanaelsteven@gmail.com>
2024-09-10 14:26:14 -0600

    length stuff

---
+++ get-length-info.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+for dir in results/*; do
+    for file in "$dir"/*-logs.txt; do
+        sol=$(basename "$file")
+        IFS='-' read -r solver _ <<< "$sol"
+        java FindMax "$file" > "$dir/$solver-lengths.txt"
+    done
+done
