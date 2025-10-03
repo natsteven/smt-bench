23b12f7ebac4aaf79111930e503801192888a60c
natsteven <nathanaelsteven@u.boisestate.edu>
2025-10-03 15:15:02 -0600

    ...

--- run_solver.sh
+++ run_solver.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 solver="$1"
 file="$2"
 
-echo "Solver=$solver File=$file"
-
 mkdir -p logs logs/"$solver"
 
 log="logs/$solver/$(basename "$file").log"
@@ -15,11 +13,12 @@ echo -n "$solver, $(basename "$file"), "
 
 start=$(date +%s.%3N)
 
-/usr/bin/time -f 'real=%e\nuser=%U\nsys=%S\nmax_rs_kb=%M' \
+rc=0
+/usr/bin/time -f 'real=%e\nuser=%U\nsys=%S\nmax_rss_kb=%M' \
   -o "$performance_log" \
   timeout 2m ./bin/"$solver" "$file"> "$log" 2>&1 || rc=$?
 
-if [ $? -eq 124 ]; then
+if [ $rc -eq 124 ]; then
 	echo "Timeout: Solver $solver exceeded 2 minutes on file $(basename "$file")" > "$performance_log"
 fi
 
